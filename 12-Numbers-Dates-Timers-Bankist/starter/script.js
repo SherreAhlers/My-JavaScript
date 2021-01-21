dfdf5eb3f32a7006249bfe9a95b6978b49d33c41
SherreAhlers <sherre.ahlers@gmail.com>
2021-01-20 19:20:46 -0600

    International numbers

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -17,11 +17,11 @@ const account1 = {
 
   movementsDates: [
     '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
+    '2021-01-16T07:42:02.383Z',
+    '2021-01-28T09:15:04.904Z',
+    '2021-01-17T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
+    '2020-01-19T17:01:17.194Z',
     '2020-07-11T23:36:17.929Z',
     '2020-07-12T10:51:36.790Z',
   ],
@@ -81,19 +81,45 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
-const displayMovements = function (movements, sort = false) {
+const formatMovementDate = function (date, locale) {
+  const calcDaysPassed = (date1, date2) =>
+    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
+  const daysPassed = calcDaysPassed(new Date(), date);
+  // console.log(daysPassed);
+
+  if (daysPassed === 0) return 'Today';
+  if (daysPassed === 1) return 'Yesterday';
+  if (daysPassed <= 7) return `${daysPassed} days ago`;
+
+  // const day = `${date.getDate()}`.padStart(2, 0);
+  // const month = `${date.getMonth() + 1}`.padStart(2, 0);
+  // const year = date.getFullYear();
+  // return `${day}/${month}/${year}`;
+  return new Intl.DateTimeFormat(locale).format(date);
+};
+
+const displayMovements = function (acc, sort = false) {
   containerMovements.innerHTML = '';
 
-  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+  const movs = sort
+    ? acc.movements.slice().sort((a, b) => a - b)
+    : acc.movements;
 
   movs.forEach(function (mov, i) {
+    // calling for each on movements
     const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const date = new Date(acc.movementsDates[i]);
+
+    const displayDate = formatMovementDate(date, acc.locale);
 
     const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
+    <div class="movements__date">${displayDate}</div>
         <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
@@ -142,7 +168,7 @@ createUsernames(accounts);
 
 const updateUI = function (acc) {
   // Display movements
-  displayMovements(acc.movements);
+  displayMovements(acc);
 
   // Display balance
   calcDisplayBalance(acc);
@@ -154,6 +180,38 @@ const updateUI = function (acc) {
 ///////////////////////////////////////
 // Event handlers
 let currentAccount;
+
+// FAKE ALWAYS LOGGED IN
+currentAccount = account1;
+updateUI(currentAccount);
+containerApp.style.opacity = 100;
+
+// EXPERAMENTING with API - to get differecnt codes google iso language code table - click one that has lingos.net
+
+// const now1 = new Date();
+// const options = {
+//   hour: 'numeric',
+//   minute: 'numeric',
+//   day: 'numeric',
+//   month: 'long', // long will give us the full name of month
+//   year: 'numeric',
+//   weekday: 'long', // this will spell out the whole name of week
+//   // can also use short
+// };
+// const locale = navigator.language;
+// console.log(locale);
+// // this will output: en-US
+
+// // for UK
+// labelDate.textContent = new Intl.DateTimeFormat(locale, options).format(now1);
+
+// for US
+// labelDate.textContent = new Intl.DateTimeFormat('en-Us').format(now1);
+// the above will change date depending on where date is being requested
+
+// for Syria
+// for UK
+// labelDate.textContent = new Intl.DateTimeFormat('ar-SY').format(now1);
 
 btnLogin.addEventListener('click', function (e) {
   // Prevent form from submitting
@@ -172,6 +230,39 @@ btnLogin.addEventListener('click', function (e) {
       currentAccount.owner.split(' ')[0]
     }`;
     containerApp.style.opacity = 100;
+
+    // CREATE current date and time
+    const now1 = new Date();
+    const options = {
+      hour: 'numeric',
+      minute: 'numeric',
+      day: 'numeric',
+      month: 'numeric', // long will give us the full name of month
+      year: 'numeric',
+      // weekday: 'long', // this will spell out the whole name of week
+      // can also use short
+    };
+    // const locale = navigator.language;
+    // console.log(locale);
+    // this will output: en-US
+
+    // for UK
+    labelDate.textContent = new Intl.DateTimeFormat(
+      currentAccount.locale,
+      options
+    ).format(now1);
+    // this means right now as far as date goes
+    // day/month/year - for user in Portugal
+    // const day = `${now1.getDate()}`.padStart(2, 0);
+    // const month = `${now1.getMonth() + 1}`.padStart(2, 0);
+    // const year = now1.getFullYear();
+    // const hour = `${now1.getHours()}`.padStart(2, 0);
+    // const min = `${now1.getMinutes()}`.padStart(2, 0);
+    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
+    // console.log(labelDate);
+
+    // month/day/year - for users in US
+    // labelDate.textContent = `${month}/${day}/${year}, ${hour}:${min}`;
 
     // Clear input fields
     inputLoginUsername.value = inputLoginPin.value = '';
@@ -201,6 +292,11 @@ btnTransfer.addEventListener('click', function (e) {
     currentAccount.movements.push(-amount);
     receiverAcc.movements.push(amount);
 
+    // ADD TRANSFER DATE
+    currentAccount.movementsDates.push(new Date().toISOString());
+    // javascript will format this date as a string
+    receiverAcc.movementsDates.push(new Date().toISOString());
+
     // Update UI
     updateUI(currentAccount);
   }
@@ -215,6 +311,9 @@ btnLoan.addEventListener('click', function (e) {
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
     currentAccount.movements.push(amount);
+
+    // ADD LOAN DATE
+    currentAccount.movementsDates.push(new Date().toISOString());
 
     // Update UI
     updateUI(currentAccount);
@@ -645,3 +744,18 @@ const future = new Date(2037, 10, 19, 15, 23);
 future.setFullYear(2040);
 // console.log(future);
 // this will output: Mon Nov 19 2040 15:23:00 GMT-0600 (Central Standard Time)
+
+// OPERATIONS WITH DATES
+// CALCULATIONS
+const future1 = new Date(2037, 10, 19, 15, 23);
+// console.log(+future1);
+// this will output:
+// 2142278580000
+
+const calcDaysPassed = (date1, date2) =>
+  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
+// console.log(days1);
+// this will output:
+// 10
